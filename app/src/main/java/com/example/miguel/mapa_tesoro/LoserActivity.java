a1adaaa32e46d3cbc0668f04fe12a5aae6ab7922
Miguel <msanchezblanco@danielcastelao.org>
2018-01-18 16:47:34 +0100

    Modificadas las Activitys de Ganar y Perder con su respectivo codigo para reiniciar hasta la Activity principal o a la del mapa con sus respectivos botones

--- app/src/main/java/com/example/miguel/mapa_tesoro/LoserActivity.java
+++ app/src/main/java/com/example/miguel/mapa_tesoro/LoserActivity.java
@@ -1,13 +1,97 @@
 package com.example.miguel.mapa_tesoro;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoserActivity extends AppCompatActivity {
+
+
+    String cadena = "¿Seguro que quieres irte ya? La verdad lo veia venir...";
+
+    public static final int INTERVALO = 2000; //2 segundos para salir
+    public long tiempoPrimerClick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loser);
+
+        Button btn = (Button) findViewById(R.id.bReintentar);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), MapsActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+
+
+    public void salir (View view){ //para salir de la aplicacion con un dialogo de confirmacion
+
+        //se prepara la alerta creando nueva instancia
+        AlertDialog.Builder alertbox = new AlertDialog.Builder(this);
+        //seleccionamos la cadena a mostrar
+        alertbox.setMessage(cadena);
+        //elegimos un positivo SI y creamos un Listener
+        alertbox.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent salida=new Intent( Intent.ACTION_MAIN); //Llamando a la activity principal
+                finish();
+            }
+        });
+
+        //elegimos un positivo NO y creamos un Listener
+        alertbox.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+
+        //mostramos el alertbox
+        alertbox.show();
+
+    }
+
+
+    public void onBackPressed(){
+
+        //se prepara la alerta creando nueva instancia
+        AlertDialog.Builder alertbox = new AlertDialog.Builder(this);
+        //seleccionamos la cadena a mostrar
+        alertbox.setMessage(cadena);
+
+        if (tiempoPrimerClick + INTERVALO > System.currentTimeMillis()){
+            super.onBackPressed();
+            return;
+        }else{
+            alertbox.setMessage(cadena);
+            //elegimos un positivo SI y creamos un Listener
+            alertbox.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Intent salida=new Intent( Intent.ACTION_MAIN); //Llamando a la activity principal
+                    finishAffinity();
+                }
+            });
+
+            //elegimos un positivo NO y creamos un Listener
+            alertbox.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                }
+            });
+        }
+        //mostramos el alertbox
+        alertbox.show();
     }
 }
